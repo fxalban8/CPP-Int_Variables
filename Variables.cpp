d006efbdf7557f97b8ce0d63e2559fa29ce8814d
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 09:31:31 -0500

    Initializing the two variables in a single line

--- Variables.cpp
+++ Variables.cpp
@@ -3,9 +3,8 @@ using namespace std;
 
 int main(){
 
-	int numberFruits =3;
-	int numberVegetables=5;
-
+	int numberFruits =3, numberVegetables=5;
+	
 	cout<<"The number of fruits is "<<numberFruits<<" and the number of vegetables is "<<numberVegetables<<endl;
 	return 0;
 }
