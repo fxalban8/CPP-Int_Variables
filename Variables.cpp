e38fe1c2f6d6199541bf47fd2ac11da13dd8fe37
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 09:34:33 -0500

    We have just added the number of products sold

--- Variables.cpp
+++ Variables.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main(){
 
 	int numberFruits =3, numberVegetables=5;
+	int numberProducts= numberFruits+numberVegetables;
 	
-	cout<<"The number of fruits is "<<numberFruits<<" and the number of vegetables is "<<numberVegetables<<endl;
+	cout<<"The number of fruits is "<<numberFruits<<" and the number of vegetables is "<<numberVegetables<<"\n The number of products is "<<numberProducts<<endl;
 	return 0;
 }
