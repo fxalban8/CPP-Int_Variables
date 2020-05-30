247d7d3a253b65ea3109f12c060f30858c462b31
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 09:29:58 -0500

    Initializing variables and printing them

--- Variables.cpp
+++ Variables.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main(){
 
+	int numberFruits =3;
+	int numberVegetables=5;
 
+	cout<<"The number of fruits is "<<numberFruits<<" and the number of vegetables is "<<numberVegetables<<endl;
 	return 0;
 }
